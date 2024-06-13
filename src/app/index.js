d42450bb7ae2ac72c235c05f0a289035a24f3602
Lee Brasil <lee.brasil@outlook.com>
2024-06-13 20:48:14 -0300

    update

--- src/app/index.js
+++ src/app/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import Tabs from './components/Tabs';
 import { COLORS } from './components/Theme';
-
+import { StatusBar } from 'expo-status-bar';
 const App = () => {
 
   return (
     <SafeAreaView style={{flex:1}}>
+       <StatusBar backgroundColor={COLORS.statusbar} barStyle="light-content" />
        <Tabs />
     </SafeAreaView>
   );
